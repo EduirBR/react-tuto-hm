ae1971a8b644cbcd9c6df05fabef50f9ea33f91d
Eduir Brazon <eduirbrazon@gmail.com>
2024-11-20 19:38:57 -0400

    apply useState

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 type Props = {
     data: string[];
 };
 
 function List({ data }: Props) {
-    const handleClick = (e: string) => {
+    //state hook
+    // regresa el valor seleccionado y la funcion para modificarlo
+    // se inicializa en -1 para que no tenga ninguno seleccionado
+    const [selected, setIndex] = useState(-1);
+    const handleClick = (e: string, index: number) => {
+        setIndex(index);
         console.log(e);
     };
 
@@ -11,9 +18,11 @@ function List({ data }: Props) {
         <ul className="list-group">
             {data.map((d, index) => (
                 <li
-                    onClick={() => handleClick(d)}
+                    onClick={() => handleClick(d, index)}
                     key={d}
-                    className="list-group-item"
+                    className={`list-group-item ${
+                        selected == index ? "active" : ""
+                    }`}
                 >
                     {index + 1}. {d}
                 </li>
