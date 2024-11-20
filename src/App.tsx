65f80f616fa1797be4b35e09dc466fe06cb7d41e
Eduir Brazon <eduirbrazon@gmail.com>
2024-11-20 09:36:08 -0400

    children componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import Card from "./components/Card";
+import Card, { CardBody } from "./components/Card";
 
 function App() {
-    return <Card />;
+    return (
+        <Card>
+            <CardBody
+                title={"This is the title"}
+                bodyText={"This is the body text"}
+            />
+        </Card>
+    );
 }
 
 export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,15 @@
-import { CSSProperties } from "react";
+import { CSSProperties, ReactNode } from "react";
+interface CardBodyProps {
+    title: String;
+    bodyText: String;
+}
+interface CardProps {
+    children: ReactNode; // children se usa para pasar otro componente
+}
 
-function Card() {
+function Card(props: CardProps) {
+    const { children: body } = props;
+    //otra forma de dart estilos al componente
     const cardStyle: CSSProperties = {
         width: "350px",
         margin: "10px",
@@ -8,21 +17,17 @@ function Card() {
 
     return (
         <div className="card" style={cardStyle}>
-            <div className="card-body">
-                <CardBody />
-            </div>
+            <div className="card-body">{body}</div>
         </div>
     );
 }
 
-function CardBody() {
+export function CardBody(props: CardBodyProps) {
+    const { title, bodyText } = props;
     return (
         <>
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick text as example here</p>
-            <a href="" className="btn btn-primary">
-                Go somewhere
-            </a>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{bodyText}</p>
         </>
     );
 }
